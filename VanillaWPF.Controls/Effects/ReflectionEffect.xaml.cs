9fdfd2a69c9e0b7b512db582ee790395ab1778aa
isami <37741394+Isami-z@users.noreply.github.com>
2023-10-01 21:12:43 +0800

    add mvvmtoolkit and some effects with a lineargradientbutton

---
+++ VanillaWPF.Controls/Effects/ReflectionEffect.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VanillaWPF.Controls.Effects
+{
+    /// <summary>
+    /// Interaction logic for ReflectionEffect.xaml
+    /// </summary>
+    public partial class ReflectionEffect : UserControl
+    {
+        public ReflectionEffect()
+        {
+            InitializeComponent();
+        }
+    }
+}
